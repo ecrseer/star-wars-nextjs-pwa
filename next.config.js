439de8177d8f6c16a5909410f461db24891e2e53
ecrseer <juustino@live.com>
2021-01-05 20:12:26 -0300

    ex

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ async function headers() {
     return [
       {
         // matching all API routes
-        source: "https://swapi.dev/api/:path*",
+        source: "/swapi.dev/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://swapi.dev/api/" },

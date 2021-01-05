9a9f8a191389bc7c4c3cdee89c48df56255681bb
ecrseer <juustino@live.com>
2021-01-05 16:39:56 -0300

    grd test

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,17 @@
 import Head from 'next/head';
-
 import { useEffect, useState } from 'react';
-import swapi from '../api';
+import GridList from '../src/components/GridList/GridList';
 
 
 
 export default function index() {
-    async function acessarFilmes(){
-        try{
-        const {data} = await swapi.get('/films/1',{headers:{
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Authorization", 
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
-            "Content-Type": "application/json;charset=UTF-8"  
-        }
-        });
-            setDados(''+data.title);
-            console.log(dados);
-        }catch(er){
-            console.log(er);
-        }
-    }
+    
 
-
-    const [dados,setDados] = useState('...');
-
-    useEffect(()=>{
-        acessarFilmes();
-    })
 
     return(
     <div>        
-        <h1 >Starwars</h1>
-        <h2>{dados}</h2>
+        <GridList/>
     </div>
     );
+    
 }
---
+++ src/components/GridList/GridList.js
@@ -0,0 +1,33 @@
+import swapi from '../../../api';
+import { useEffect, useState } from 'react';
+
+const GridList=()=>{
+
+const [dados,setDados] = useState('...');
+    useEffect(()=>{
+        
+    })
+
+    
+
+    async function acessarFilmes(){
+        try{
+        const {data} = await swapi.get('/films/2',{headers:{
+            "Access-Control-Allow-Origin": "https://swapi.dev/",
+            
+        }
+        });
+            setDados(''+data.title);
+            console.log(dados);
+        }catch(er){
+            console.log(er);
+        }
+    }
+    return(<div>        
+        <h1 >Starwars</h1>
+        <h2>{dados}</h2>
+        <button onClick={acessarFilmes}>aa</button>
+    </div>)
+}
+
+export default GridList;

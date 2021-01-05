f07c8fa2be7ac66babde14a1e4118fb4eabf53fe
ecrseer <juustino@live.com>
2021-01-05 15:51:04 -0300

    testando api

--- api.js
+++ api.js
@@ -1,2 +1,13 @@
 //importando biblioteca axios
 import axios from 'axios';
+
+//http de onde sera consumida
+let basURL="https://swapi.dev/api/";
+
+let axconfig = { 
+    baseURL:basURL
+}
+//cria conexao com axios
+const swapi = axios.create(axconfig);
+
+export default swapi;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head';
 
 import { useEffect, useState } from 'react';
+import swapi from '../api';
 
 export default function index() {
+    const [dados,setDados] = useState('...');
+
+    useEffect(async()=>{
+        try{
+        const {data} = await swapi.get('/films/1');
+            setDados(''+data.title);
+            console.log(dados);
+        }catch(er){
+            console.log(er);
+        }
+    })
+
     return(
     <div>        
         <h1 >Starwars</h1>
+        <h2>{dados}</h2>
     </div>
     );
 }

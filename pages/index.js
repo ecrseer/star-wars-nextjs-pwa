c91f11ccc9cb3c1614ec5ea9490df70884d5b2f2
ecrseer <juustino@live.com>
2021-01-05 16:28:50 -0300

    headers para permitir acessar de outra api

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,30 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import swapi from '../api';
 
-export default function index() {
-    const [dados,setDados] = useState('...');
 
-    useEffect(async()=>{
+
+export default function index() {
+    async function acessarFilmes(){
         try{
-        const {data} = await swapi.get('/films/1');
+        const {data} = await swapi.get('/films/1',{headers:{
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Authorization", 
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+            "Content-Type": "application/json;charset=UTF-8"  
+        }
+        });
             setDados(''+data.title);
             console.log(dados);
         }catch(er){
             console.log(er);
         }
+    }
+
+
+    const [dados,setDados] = useState('...');
+
+    useEffect(()=>{
+        acessarFilmes();
     })
 
     return(

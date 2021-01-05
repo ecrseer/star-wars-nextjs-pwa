f1b22af4134e8ec3d3f7038a1a3fe5fc76d1f59b
ecrseer <juustino@live.com>
2021-01-05 20:04:49 -0300

    next headers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
 const withPWA = require('next-pwa')
+const wPWAProperties = withPWA({
+    pwa: {
+      dest: 'public',
+      swSrc: 'service-worker.js'
+    },
+    env:{      
+        MY_SWAPI: process.env.MY_SWAPI,
+    },
+    
+    
+  });
+async function headers() {
+    return [
+      {
+        // matching all API routes
+        source: "https://swapi.dev/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://swapi.dev/api/" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+    }
+]}
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    swSrc: 'service-worker.js'
-  },
-  env:{
-      
-      MY_SWAPI: process.env.MY_SWAPI,
-  }
-})
+module.exports = {headers,wPWAProperties};
+
+

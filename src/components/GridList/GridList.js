fb2377d3b4e67bb4d4a558451d2ef6c543b12d41
ecrseer <juustino@live.com>
2021-01-05 21:38:24 -0300

    deploy2test

--- src/components/GridList/GridList.js
+++ src/components/GridList/GridList.js
@@ -15,14 +15,7 @@ const GridList = ({data}) =>{
         console.log("DD "+process.env.NODE_ENV);
             console.log("SWW "+process.env.MY_SWAPI);
         try{
-        const {data} = await swapi.get('/films/2',{
-            headers:{
-            'Content-Type': 'application/json',                
-            'Access-Control-Allow-Origin': 'https://swapi.dev/api',
-            'Access-Control-Allow-Methods' : 'GET,HEAD,OPTIONS',
-            'Access-Control-Allow-Headers' : 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
-        }
-        });
+        const {data} = await swapi.get('/films/2');
         
             setDados(''+data.title);
             console.log(dados);

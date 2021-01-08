9407191ed68e0608676506728376c2cf49f540a2
ecrseer <juustino@live.com>
2021-01-08 18:16:38 -0300

    modularizando component card

---
+++ src/components/PeopleList/CardPersonagens.js
@@ -0,0 +1,81 @@
+import { makeStyles, Paper, Typography } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import {PersonagemImgApi} from '../../assets/personagensImageApi';
+
+const useStyles = makeStyles((theme) => ({
+    root: {     
+        display:'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(220px, 0.2fr))',
+        gridGap: '10px',
+        justifyContent: 'center'
+    },
+}));
+
+
+
+const CardPersonagens = ({SwapiDados})=>{  
+        
+    const classes = useStyles();
+    const [personagensImgs,setPersonagensImgs] = useState([{}]);
+
+    async function getTodosImgApi(){
+
+        try {
+            const {data} = await PersonagemImgApi.get('/all.json');
+            //console.log('imagemdo'+data[1].name)
+            setPersonagensImgs(data);
+            
+            }catch (error) {
+                console.log('0 images porque '+error)        
+            } 
+        }
+
+        function getImgPersonagem(nomePersonaCard){
+            let personagemMesmoNome = personagensImgs
+                .filter(personag=>personag.name==nomePersonaCard);
+             if(typeof personagemMesmoNome[0] === 'undefined')
+                return('#')
+        
+             return(personagemMesmoNome[0].image);
+             
+         }
+
+
+
+    useEffect(()=>{
+        getTodosImgApi();
+    },
+    [SwapiDados])
+
+
+    
+    return(
+    <div className={classes.root}>{
+      SwapiDados.map((personagem,index)=>{     
+        return(
+            <Paper elevation={4}
+        variant="elevation" 
+        style={{paddingLeft:'12px'
+        ,backgroundColor:'#111'}}               
+           key={index}
+           color="secondary">
+             <img src={getImgPersonagem(personagem.name)}
+             width="32%"/>
+             <Typography color="primary">
+               <div>{personagem.name}</div>
+               
+               <div>altura: {personagem.height}</div>
+               <div>massa: {personagem.mass}</div>
+               <div>cor do cabelo: {personagem.hair_color}</div>
+               <div>cor da pele: {personagem.skin_color}</div>
+               <div>cor dos olhos: {personagem.eye_color}</div>
+               <div>data de nascimento: {personagem.birth_year}</div>
+               <div>genero: {personagem.gender}</div>
+               </Typography> 
+           </Paper>
+
+      )})}</div>)            
+        
+    }
+
+export default CardPersonagens;
--- src/components/PeopleList/PeopleList.js
+++ src/components/PeopleList/PeopleList.js
@@ -1,9 +1,8 @@
-import {swapi} from '../../../api';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Box, GridList, Grow, makeStyles, Paper, Typography } from '@material-ui/core';
-import {PersonagemImgApi} from '../../assets/personagensImageApi';
-import PeopleCard from './PeopleCard';
+import { Grow, makeStyles, } from '@material-ui/core';
+
+import CardPersonagens from './CardPersonagens';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,32 +16,13 @@ const useStyles = makeStyles((theme) => ({
 
 
 const PeopleList = (props) =>{
-    const classes = useStyles();
+    
 
     const [dados,setDados] = useState([]);    
-    const [personagensImgs,setPersonagensImgs] = useState([{}]);
+    
     const [isCarregando,setIsCarregando] = useState(false);
 
-    function getImgPersonagem(nomePersonaCard){
-        let personagemMesmoNome = personagensImgs
-            .filter(personag=>personag.name==nomePersonaCard);
-         if(typeof personagemMesmoNome[0] === 'undefined')
-            return('#')
-
-         return(personagemMesmoNome[0].image);
-         
-     }
- async function getTodosImgApi(){
-    
-    try {
-        const {data} = await PersonagemImgApi.get('/all.json');
-        //console.log('imagemdo'+data[1].name)
-        setPersonagensImgs(data);
-        
-        }catch (error) {
-            console.log('0 images porque '+error)        
-        } 
- }
+  
 
 
 async function acessaPersonagens(arrLinks){    
@@ -50,8 +30,6 @@ async function acessaPersonagens(arrLinks){
     setDados([]);    
     
     if(arr.length>1){  
-             
-
     for (var i = 0; i < arr.length; i++) { 
         try{            
             
@@ -66,40 +44,12 @@ async function acessaPersonagens(arrLinks){
    }
 
    useEffect(()=>{  
-    getTodosImgApi();
+    
     acessaPersonagens(props.personagensfilme);     
 },
 [props.personagensfilme])
 
-const CardPersonagem = ()=>{         
-        return(
-        <div className={classes.root}>{
-          dados.map((personagem,index)=>{     
-            return(
-                <Paper elevation={4}
-            variant="elevation" 
-            style={{paddingLeft:'12px'
-            ,backgroundColor:'#111'}}               
-               key={index}
-               color="secondary">
-                 <img src={getImgPersonagem(personagem.name)}
-                 width="32%"/>
-                 <Typography color="primary">
-                   <div>{personagem.name}</div>
-                   
-                   <div>altura: {personagem.height}</div>
-                   <div>massa: {personagem.mass}</div>
-                   <div>cor do cabelo: {personagem.hair_color}</div>
-                   <div>cor da pele: {personagem.skin_color}</div>
-                   <div>cor dos olhos: {personagem.eye_color}</div>
-                   <div>data de nascimento: {personagem.birth_year}</div>
-                   <div>genero: {personagem.gender}</div>
-                   </Typography> 
-               </Paper>
 
-          )})}</div>)            
-            
-        }
             
             
 
@@ -107,7 +57,7 @@ const CardPersonagem = ()=>{
 return(
  <Grow in={!isCarregando} {...props}> 
   <div >
-     <CardPersonagem/>
+     <CardPersonagens SwapiDados={dados}/>
    </div>
  </Grow>
  )}

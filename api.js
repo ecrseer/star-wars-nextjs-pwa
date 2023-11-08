b3c3326266790b59f7aac7357c6019614f46ac6c
ecrseer <juustino@live.com>
2023-11-07 21:43:15 -0300

    fix heroku down

--- api.js
+++ api.js
@@ -1,17 +1,12 @@
 //importando biblioteca axios
-import axios from 'axios';
+import axios from "axios";
 
-//http de onde sera consumida
-/* tive que fazer um proxy pra poderbypassar
- o problema de cross origin no deploy pra vercel */
-let basURL= process.env.NODE_ENV == 'production'? 
-"https://swapi-g4b-proxy.herokuapp.com/":
-"https://swapi.dev/api";
+let basURL = "https://swapi.dev/api";
 
-let axconfig = { 
-    baseURL:basURL
-}
+let axconfig = {
+  baseURL: basURL,
+};
 //cria conexao com axios
 const swapi = axios.create(axconfig);
 
-module.exports={swapi,axios};
+module.exports = { swapi, axios };
--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const withPWA = require('next-pwa')
-
- 
-  
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-    pwa: {
-      dest: 'public',
-      swSrc: 'service-worker.js'
-    },
-    env:{      
-        /* achei que ia ser necessario passar
-        a url da api como variavel local
-         mas acabei nao precisando(por enquanto) */
-        MY_SWAPI: process.env.MY_SWAPI,
-    },
-    
-    
-  });
-
+  dest: "public",
+  swSrc: "service-worker.js",
+});
